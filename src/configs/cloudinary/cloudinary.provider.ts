ca048ce4ea9f6779751730e5427ef1a97aed4726
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2026-02-06 11:07:14 +0700

    [Update] change cloudinary

--- src/configs/cloudinary/cloudinary.provider.ts
+++ src/configs/cloudinary/cloudinary.provider.ts
@@ -6,9 +6,9 @@ export const CloudinaryProvider = {
   provide: 'CLOUDINARY',
   useFactory: () => {
     return cloudinary.config({
-      cloud_name: 'ddv4cb3u5',
-      api_key: '268716551999716',
-      api_secret: 'LPthiy7GQBO1q50VRIPKV7L-0LA',
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
     });
   },
 };
